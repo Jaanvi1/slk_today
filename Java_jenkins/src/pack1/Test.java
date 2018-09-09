db44e1198acc7e45f5e0022ff01857bf7df441e3
jyo <jyo@gmail.com>
2018-09-09 12:58:15 +0530

    commit java project

---
+++ Java_jenkins/src/pack1/Test.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package pack1;
+
+/**
+ * @author slk
+ *
+ */
+public class Test {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Jyothi");
+
+	}
+
+}
+ 
